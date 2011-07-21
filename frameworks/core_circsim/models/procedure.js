b80f48a444d58ef5f0122f6cc641dabdc7df7ede
Ryan Golden <ryan.john.golden@gmail.com>
2011-07-21 09:18:11 -0500

    Added default value to isComplete property of procedure

--- frameworks/core_circsim/models/procedure.js
+++ frameworks/core_circsim/models/procedure.js
@@ -3,8 +3,10 @@
 CoreCircsim.Procedure = SC.Record.extend({
 
   title: SC.Record.attr(String),
-  isComplete: SC.Record.attr(Boolean),
-  
+  isComplete: SC.Record.attr(Boolean, {
+    defaultValue: false
+  }),
+
   grid: SC.Record.toOne('CoreCircsim.Grid')
 
 });

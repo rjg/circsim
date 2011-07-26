752919f6bb6800c3f0268ac8b07b252ea557964c
Ryan Golden <ryan.john.golden@gmail.com>
2011-07-26 18:16:12 -0500

    Added isHighlighted property to CoreCircsim.Cell class to manage appearance during evaluations.

--- frameworks/core_circsim/models/cell.js
+++ frameworks/core_circsim/models/cell.js
@@ -3,6 +3,7 @@
 CoreCircsim.Cell = SC.Record.extend({
   
   value: SC.Record.attr(String),
+  isHighlighted: SC.Record.attr(Boolean),
   
   column: SC.Record.toOne('CoreCircsim.Column', {
     isMaster: NO

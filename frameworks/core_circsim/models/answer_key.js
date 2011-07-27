3c1f2d7a51a48442ee2d3bab5d14fd84d38e6968
Ryan Golden <ryan.john.golden@gmail.com>
2011-07-27 16:59:11 -0500

    Created CoreCircsim.AnswerKey class to manage data associated with procedure specific answer keys.

---
+++ frameworks/core_circsim/models/answer_key.js
@@ -0,0 +1,17 @@
+/*globals CoreCircsim*/
+
+CoreCircsim.AnswerKey = SC.Record.extend({
+  
+  highlights: SC.Record.attr(Array),
+  category: SC.Record.attr(String),
+  isCorrect: SC.Record.attr(Boolean),
+  comment: SC.Record.attr(String),
+  cells: SC.Record.attr(Array),
+  cellValues: SC.Record.attr(Array),
+  column: SC.Record.attr(Number),
+
+  procedure: SC.Record.toOne('CoreCircsim.Procedure', {
+    isMaster: NO
+  })
+
+});

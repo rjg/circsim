f74939d0b945f56bf9f87439bb53af49c14b7dca
Ryan Golden <ryan.john.golden@gmail.com>
2011-07-27 17:43:15 -0500

    Added comments to answer key fixture to clarify some of the properties.

--- frameworks/core_circsim/fixtures/answer_key.js
+++ frameworks/core_circsim/fixtures/answer_key.js
@@ -3,14 +3,14 @@
 sc_require('models/answer_key');
 
 CoreCircsim.AnswerKey.FIXTURES = [{   
-  guid: 1,
-  procedure: 1,
-  column: 0,
-  highlights: [0,3],
-  cells: [0,3],
-  cellValues: [0,0],  
-  isCorrect: YES,
-  category: "Neural Variables",
+  guid: 1, 
+  procedure: 1, // Which procedure this answer key belongs to.
+  column: 0, // Which column number these are relevant to.  In this case, DR, the first column.
+  highlights: [0,3], // These are the cells we want to highlight when presenting the summaries...
+  cells: [0,3], // This array represents the rows that we want to compare.  The numbers are indices of the rows, so in this case, the first and fourth row... (IS & HR)
+  cellValues: [0,0], // This array represents the values that we want in those rows... 
+  isCorrect: YES, // Is this is correct or incorrect answer? (For display purposes)
+  category: "Neural Variables", 
   comment: "Right!  IS and HR are both controlled by the nervous system (they’re neural variables).  By definition, the DR interval occurs BEFORE any changes in neural firing occurs.  So, there are no changes to these two variables."
 },
 {  

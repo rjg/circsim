31fd7ac75bb9f8df6c25db887e74abc0ee8147b7
Ryan Golden <ryan.john.golden@gmail.com>
2011-07-29 12:39:27 -0500

    Commented out automatic selection of first procedure.

--- apps/circsim/main.js
+++ apps/circsim/main.js
@@ -9,8 +9,8 @@ Circsim.main = function main() {
   Circsim.proceduresController.set('content', CoreCircsim.store.find(CoreCircsim.Procedure));
 
   // Artificially select of first procedure in proceduresController for time being...
-  var procedure = Circsim.proceduresController.get('content').firstObject();
-  Circsim.proceduresController.selectObject(procedure);
+  // var procedure = Circsim.proceduresController.get('content').firstObject();
+  // Circsim.proceduresController.selectObject(procedure);
 
 };
 

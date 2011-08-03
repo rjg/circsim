ad758d05de850aec7d5d8127c8f26de71f056fc3
Ryan Golden <ryan.john.golden@gmail.com>
2011-08-03 16:31:35 -0500

    Removed boilerplate code from core.js.

--- apps/circsim/core.js
+++ apps/circsim/core.js
@@ -1,27 +1,8 @@
-// ==========================================================================
-// Project:   Circsim
-// Copyright: @2011 My Company, Inc.
-// ==========================================================================
-/*globals Circsim */
+/*globals Circsim*/ 
 
-/** @namespace
-
-  My cool new app.  Describe your application.
-  
-  @extends SC.Object
-*/
-Circsim = SC.Application.create(
-  /** @scope Circsim.prototype */ {
+Circsim = SC.Application.create({
 
   NAMESPACE: 'Circsim',
-  VERSION: '0.1.0',
-
-  // This is your application store.  You will use this store to access all
-  // of your model data.  You can also set a data source on this store to
-  // connect to a backend server.  The default setup below connects the store
-  // to any fixtures you define.
-  store: SC.Store.create().from(SC.Record.fixtures)
+  VERSION: '0.1.0'
   
-  // TODO: Add global constants or singleton objects needed by your app here.
-
-}) ;
+});

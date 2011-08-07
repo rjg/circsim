f959bc17abfb363120e6c925ddbec4a02959354c
Ryan Golden <ryan.john.golden@gmail.com>
2011-08-07 13:34:17 -0400

    Added instructions property to procedure model

--- frameworks/core_circsim/fixtures/procedure.js
+++ frameworks/core_circsim/fixtures/procedure.js
@@ -9,6 +9,7 @@ CoreCircsim.Procedure.FIXTURES = [
   guid: '1',
   title: "Decrease Ra",
   introduction: "This is the introduction to the procedure.",
+  instructions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consectetur purus eget ipsum fermentum in mattis arcu hendrerit. Etiam ac nulla scelerisque eros facilisis tempus sit amet in erat. Nam sed nunc at ligula faucibus vehicula. Nunc nec elementum risus. Aenean venenatis congue faucibus. Ut et massa arcu. Curabitur turpis justo, aliquet et tristique a, tristique eu libero. Morbi euismod felis ut eros vestibulum in posuere dui auctor. Donec sapien enim, commodo non ornare nec, tempus quis lorem. Cras eget fermentum tortor. Donec sed quam nunc. Quisque dui tellus, sollicitudin vulputate bibendum vulputate, ornare ac massa. Praesent sit amet augue sit amet odio hendrerit auctor et sit amet metus. Cras eget ipsum id ante posuere rutrum. Nulla elementum nibh in ipsum pellentesque eget interdum tellus mollis. Mauris id ante at ante tempor aliquet. Vestibulum ut est velit, id ultricies odio. Integer auctor tristique pharetra.",
   isComplete: false,
   cols: ["DR", "RR", "SS"],
   rows: ["IS", "CVP", "SV", "HR", "CO", "Ra", "MAP"],

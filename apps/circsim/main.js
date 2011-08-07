c1dcc85e0d47f54d1ea7bf2555f1231c52d76228
Ryan Golden <ryan.john.golden@gmail.com>
2011-08-07 13:34:32 -0400

    Removed some old code.

--- apps/circsim/main.js
+++ apps/circsim/main.js
@@ -8,13 +8,9 @@ Circsim.main = function main() {
   // Set content of proceduresController
   Circsim.proceduresController.set('content', CoreCircsim.store.find(CoreCircsim.Procedure));
 
-  // Artificially select of first procedure in proceduresController for time being...
-  // var procedure = Circsim.proceduresController.get('content').firstObject();
-  // Circsim.proceduresController.selectObject(procedure);
-
   // Initialize Statechart
   Circsim.statechart.initStatechart();  
-
+  
 };
 
 function main() {

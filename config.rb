705d73e392ebdd7e1373764473e50760f9fa04d1
Ryan Golden <ryan.john.golden@gmail.com>
2011-08-08 11:06:25 -0400

    Setup compass for styling

---
+++ config.rb
@@ -0,0 +1,9 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "themes/corephysio/resources"
+sass_dir = "themes/corephysio/src"
+images_dir = "themes/corephysio/resources/images"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+asset_cache_buster :none

298a2a546be4548a24d48604a1b7153a0f5301a5
Ryan Golden <ryan.john.golden@gmail.com>
2011-08-12 13:03:52 -0400

    Created allCells computed property on cells controller.

--- apps/circsim/controllers/cells.js
+++ apps/circsim/controllers/cells.js
@@ -1,5 +1,18 @@
 /*globals Circsim */
 
 Circsim.cellsController = SC.ArrayController.create({  
-  contentBinding: "Circsim.columnController.cells"
+  
+  contentBinding: "Circsim.columnController.selection",
+
+  allCells: function() {
+    var cols     = Circsim.procedureController.get('columns'),
+        allCells = [];         
+    cols.forEach(function(col) {
+      var cells = col.get('cells');
+      cells.forEach(function(cell) {
+        allCells.pushObject(cell);
+      });
+    });
+    return allCells;
+  }.property()
 });

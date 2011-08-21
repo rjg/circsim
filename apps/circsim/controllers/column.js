9fa6da7180c95c83aee6f83e1783ddf8416d2d8b
Ryan Golden <ryan.john.golden@gmail.com>
2011-08-21 18:29:29 -0400

    Added tracking of current column to columnController.

--- apps/circsim/controllers/column.js
+++ apps/circsim/controllers/column.js
@@ -1,5 +1,6 @@
 /*globals Circsim */
 
-Circsim.columnController = SC.ObjectController.create({
-  contentBinding: SC.Binding.single('Circsim.columnsController.selection')
+Circsim.columnController = SC.Object.create({
+  content: "",
+  current: 0
 });
